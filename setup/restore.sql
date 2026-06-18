f1e7a853f42ee7611d72236200744e72da0b2360
jangalic04 <jangalic006@gmail.com>
2026-06-18 16:39:13 +0200

    Deleted all the files to upload again

--- setup/restore.sql
+++
@@ -1,9 +0,0 @@
-RESTORE DATABASE AdventureWorks
-FROM DISK = '/var/opt/mssql/data/AdventureWorks2019.bak'
-WITH
-    MOVE 'AdventureWorks2019' TO '/var/opt/mssql/data/AdventureWorks.mdf',
-    MOVE 'AdventureWorks2019_log' TO '/var/opt/mssql/data/AdventureWorks.ldf',
-    REPLACE;
-
-SELECT name, state_desc
-FROM sys.databases;
